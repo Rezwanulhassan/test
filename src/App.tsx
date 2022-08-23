ff96f5fdd835772d582b6037e198f83c21735010
rezwanul hassan <rezonsm823480@gmail.com>
2022-08-23 22:41:25 +0600

    setup project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Component,useEffect,useState} from 'react';
+import 'antd/dist/antd.css';
+import './index.css';
+import axios from 'axios';
+import { Layout,Col, Row } from 'antd';
+import { Card } from 'antd';
+const { Meta } = Card;
+const { Header, Footer, Sider, Content } = Layout;
+const App = () => {
+  
+//   useEffect(() => {
+//   Post.getPosts()
+//     .then((data) => {
+//       setArticle(data);
 
-function App() {
+//     });
+//   return () => {};
+// }, []);
+  // const [albums, setPost] = useState();
+  // useEffect(() => {
+  //   axios.get('https://api.spacexdata.com/v3/launches')
+    
+  //     .then(post => {
+  //       albums = post.data;
+  //       setPost(albums)
+  
+  //     })
+      
+  // }, [])
+  // const result = Object.keys(albums).map((key) => {
+  //   return 
+  // });
+  // const mapData = JSON.stringify([albums])
+  // console.log("sss",mapData)
+  // albums.map((item:any, i:any) => (
+  //   <li className="travelcompany-input" key={i}>
+  //     <span className="input-label">{ item.name }</span>
+  //   </li>
+  // ))} 
+//   const listItems = mapData.map((number:any,i:any) =>
+//   <li>{number}</li>
+// );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+<>
+    <Layout>
+      <Header className='app-header'>Header</Header>
+    <Row>
+      <Col span={8}><Card
+    hoverable
+    style={{margin:50}}
+    cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+  >
+    <Meta title="Europe Street beat" description="www.instagram.com" />
+  </Card></Col>
+
+    </Row>
+      <Footer className='app-footer'>Footer</Footer>
+    </Layout>
+  </>
+  )
 }
 
 export default App;
